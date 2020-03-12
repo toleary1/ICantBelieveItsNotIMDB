7e6bc8c07cd88a1d62f7bfbd3a2f9de3e80e703f
toleary1 <toleary1@gmail.com>
2020-03-12 09:04:46 -0400

    Cleaned out original template files

---
+++ src/index.js
@@ -0,0 +1,51 @@
+/*!
+
+=========================================================
+* BLK Design System React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import "assets/css/nucleo-icons.css";
+import "assets/scss/blk-design-system-react.scss?v=1.0.0";
+import "assets/demo/demo.css";
+
+import Index from "views/Index.jsx";
+import AddMovie from "views/examples/AddMovie.jsx";
+import ListMovies from "views/examples/ListMovies.jsx";
+import MoviePage from "views/examples/MoviePage.jsx";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/components" render={props => <Index {...props} />} />      
+      <Route
+        path="/add-movie"
+        render={props => <AddMovie {...props} />}
+      />
+      <Route
+        path="/list-movies"
+        render={props => <ListMovies {...props} />}
+      />
+      <Route
+        path="/movie-page"
+        render={props => <MoviePage {...props} />}
+      />
+      <Redirect from="/" to="/components" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
